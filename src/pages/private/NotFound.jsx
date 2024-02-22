ecb8b5bc261dbbbecdc7ad3e21a886cd83c2dc65
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2024-02-22 00:46:27 -0500

    adding 404

---
+++ src/pages/private/NotFound.jsx
@@ -0,0 +1,9 @@
+import { PrivateLayout } from '../../layout/PrivateLayout'
+
+export const NotFound = () => {
+  return (
+    <PrivateLayout title='Not Found'>
+      404
+    </PrivateLayout>
+  )
+}
